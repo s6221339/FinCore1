da0d9f83948503363e0a9d5d2493cb7909d0a798
Chih-Yun <chengchihyun0110@gmail.com>
2025-06-02 17:30:21 +0800

    新增entity跟Dao

---
+++ src/main/java/com/example/FinCore/entity/Family.java
@@ -0,0 +1,61 @@
+package com.example.FinCore.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "family")
+public class Family {
+	
+	@Id
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "owner")
+	private String owner;
+	
+	@Column(name = "invitor")
+	private String invitor;
+	
+	@Column(name = "create_date")
+	private LocalDate createDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public String getInvitor() {
+		return invitor;
+	}
+
+	public void setInvitor(String invitor) {
+		this.invitor = invitor;
+	}
+
+	public LocalDate getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(LocalDate createDate) {
+		this.createDate = createDate;
+	}
+	
+	
+
+}

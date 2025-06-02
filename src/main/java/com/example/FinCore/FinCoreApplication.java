4f0562a0402d8e12fb81e06bb3d4a4af8e6c3af5
s6221339 <s6221339@yahoo.com.tw>
2025-06-02 10:09:19 +0800

    first upload

---
+++ src/main/java/com/example/FinCore/FinCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.FinCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FinCore/FinCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FinCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

033495499c14ebe1de8b686585a0011be100c7d0
Chih-Yun <chengchihyun0110@gmail.com>
2025-06-03 11:15:30 +0800

    新增entity跟Dao

---
+++ src/main/java/com/example/FinCore/entity/User.java
@@ -0,0 +1,93 @@
+package com.example.FinCore.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @Column(name = "account")
+    private    String account;
+    
+    @Column(name = "name")
+    private String name;
+    
+    @Column(name = "password")
+    private String password;
+    
+    @Column(name = "phone")
+    private String phone;
+    
+    @Column(name = "avatar_url")
+    private String avatarUrl;
+    
+    @Column(name = "super_admin")
+    private boolean superAdmin;
+    
+    @Column(name = "create_date")
+    private LocalDate createDate;
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public boolean isSuperAdmin() {
+        return superAdmin;
+    }
+
+    public void setSuperAdmin(boolean superAdmin) {
+        this.superAdmin = superAdmin;
+    }
+
+    public LocalDate getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDate createDate) {
+        this.createDate = createDate;
+    }
+    
+    
+}

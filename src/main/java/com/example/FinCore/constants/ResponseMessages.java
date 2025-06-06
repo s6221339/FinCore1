540a1fd1cd8cde868e4518716781730ec19e2c84
Chih-Yun <chengchihyun0110@gmail.com>
2025-06-06 18:11:06 +0800

    更新錯誤碼

--- src/main/java/com/example/FinCore/constants/ResponseMessages.java
+++ src/main/java/com/example/FinCore/constants/ResponseMessages.java
@@ -11,14 +11,14 @@ public enum ResponseMessages {
     SERVER_ERROR(500, "伺服器錯誤，請稍後再試"),
 	MISSING_REQUIRED_FIELD(400, "缺少必要欄位"),
     DUPLICATE_MEMBER(400, "群組擁有者與邀請人不能相同"),
-    OWNER_NOT_FOUND(400, "擁有者帳號不存在"),
-    INVITOR_NOT_FOUND(400, "邀請人帳號不存在"),
+    OWNER_NOT_FOUND(404, "擁有者帳號不存在"),
+    INVITOR_NOT_FOUND(404, "邀請人帳號不存在"),
     CREATE_FAMILY_FAIL(400, "新增家族群組失敗"),
-    FAMILY_NOT_FOUND(400,"家族群組不存在"),
+    FAMILY_NOT_FOUND(404,"家族群組不存在"),
 	UPDATE_FAMILY_FAIL(400,"更新家族群組失敗"),
 	DELETE_FAMILY_FAIL(400, "刪除家庭群組失敗"),
 	NO_PERMISSION(400, "沒有 owner 權限"),
-	MEMBER_NOT_FOUND(400,"家族成員不存在");
+	MEMBER_NOT_FOUND(404,"家族成員不存在");
 
 	
 	

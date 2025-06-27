b1dde6d3fbfb5afa787a7343a9840f61d1cb015d
羊羊 <90090207as@gmail.com>
2025-06-27 11:27:35 +0800

    Secret 移除

--- src/main/java/com/example/FinCore/service/itfc/AIQueryLogsService.java
+++ src/main/java/com/example/FinCore/service/itfc/AIQueryLogsService.java
@@ -2,7 +2,5 @@ package com.example.FinCore.service.itfc;
 
 public interface AIQueryLogsService 
 {
-	
-//	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
-	
+		
 }
